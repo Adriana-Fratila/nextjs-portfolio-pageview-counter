c2830c2e24478880c786de9135e4c96dec981935
Adriana-Mariana Fratila <146179295+Adriana-Fratila@users.noreply.github.com>
2023-11-09 17:44:37 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,8 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          Étudiante en BUT informatique à l'IUT de Villetaneuse, je vous invite à découvrir mes projets universitaires.<br></br> 
+          N'hésitez pas à me contacter pour échanger, collaborer ou simplement discuter de l'univers passionnant de l'informatique.
         </h2>
       </div>
     </div>
